8e1605b85f24452c3f529fbc94ba7844fb35c452
Henry Lam <hcylam@connect.ust.hk>
2023-09-16 14:13:04 +0800

    initial app commit

---
+++ client/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Text>
+                test
+            </Text>
+        )
+    }
+}
+
+export default App
