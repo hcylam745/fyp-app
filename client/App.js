13aa61d7472b2e4540c710ba591acc3e27f423fe
Henry Lam <hcylam@connect.ust.hk>
2023-09-28 14:32:19 +0800

    figured out how to import the image, now user can move it around.

--- client/App.js
+++ client/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Map from "./Map";
-import GestureHandler from "./GestureHandler";
 import {Gesture, GestureDetector, GestureHandlerRootView} from "react-native-gesture-handler";
 
 const App = () => {
     return (
         <GestureHandlerRootView style={{flex:1}}>
-            <GestureHandler/>
+            <Map/>
         </GestureHandlerRootView>
     )
 }
--- client/Map.js
+++ client/Map.js
@@ -1,27 +1,77 @@
 import React, {Component} from "react";
-import {Animated, useWindowDimensions, View} from "react-native";
-import {Canvas, Circle, Group} from "@shopify/react-native-skia";
-import GestureHandler from "./GestureHandler";
+import {StyleSheet, View, Text, Alert, Skia} from "react-native";
+import {Canvas, Circle, Group, useImage, useValue, Image} from "@shopify/react-native-skia";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
+import { useSharedValue, useAnimatedStyle, withSpring } from "react-native-reanimated";
+import Animated from "react-native-reanimated";
+
+const img = require("./image.jpg")
+
+const styles = StyleSheet.create({
+    ball: {
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        backgroundColor: 'lightblue'
+    },
+    map: {
+      width:"100%",
+      height:"100%",
+      backgroundColor: "grey"
+    }
+});
 
 const Map = () => {
-    const size = 700;
-    const r = size * 0.33;
+  const isPressed = useSharedValue(false);
+  const offset = useSharedValue({ x: 0, y: 0});
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: offset.value.x },
+        { translateY: offset.value.y }
+      ]
+    };
+  });
+  
+  const start = useSharedValue({ x: 0, y: 0 });
+  const gesture = Gesture.Pan()
+    .onBegin(() => {
+      isPressed.value = true;
+    })
+    .onUpdate((e) => {
+      offset.value = {
+        x: e.translationX + start.value.x,
+        y: e.translationY + start.value.y
+      };
+    })
+    .onEnd(() => {
+      start.value = {
+        x: offset.value.x,
+        y: offset.value.y,
+      };
+    })
+    .onFinalize(() => {
+      isPressed.value = false;
+    });
+
+    const width = 960;
+    const height = 540;
+
+    const image = useImage(img);
+
     return (
-        <View style={{flex: 1}}>
-            <Canvas style={{ flex: 1 }}>
-                <Group blendMode="multiply">
-                    <Circle cx={r} cy={r} r={r} color="cyan" />
-                    <Circle cx={size - r} cy={r} r={r} color="magenta" />
-                    <Circle
-                    cx={size/2}
-                    cy={size - r}
-                    r={r}
-                    color="yellow"
-                    />
+      <View style={{flex:1}}>
+        <GestureDetector gesture={gesture}>
+            <Animated.View style={[styles.ball, animatedStyles]}>
+              <Canvas style={{width, height}}>
+                <Group>
+                  <Image x={0} y={0} image={image} width={width} height={height} fit="cover"/>
                 </Group>
-            </Canvas>
-        </View>
+              </Canvas>
+            </Animated.View>
+        </GestureDetector>
+      </View>
+        
     )
 }
-
 export default Map
