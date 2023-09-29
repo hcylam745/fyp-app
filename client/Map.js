4a6dacf740807b3b3c86f5f647536dd759f6ce71
Henry Lam <hcylam@connect.ust.hk>
2023-09-29 13:07:39 +0800

    added rotation and pinching functionality, can't test pinching though.

--- client/Map.js
+++ client/Map.js
@@ -22,56 +22,72 @@ const styles = StyleSheet.create({
 });
 
 const Map = () => {
-  const isPressed = useSharedValue(false);
   const offset = useSharedValue({ x: 0, y: 0});
+  const rotation = useSharedValue(0);
+  const savedRotation = useSharedValue(0);
+  const scale = useSharedValue(1);
+  const savedScale = useSharedValue(1);
   const animatedStyles = useAnimatedStyle(() => {
     return {
       transform: [
         { translateX: offset.value.x },
-        { translateY: offset.value.y }
+        { translateY: offset.value.y },
+        { rotateZ: `${(rotation.value / Math.PI)*180}deg` },
+        { scale: scale.value }
       ]
     };
   });
   
   const start = useSharedValue({ x: 0, y: 0 });
-  const gesture = Gesture.Pan()
-    .onBegin(() => {
-      isPressed.value = true;
-    })
-    .onUpdate((e) => {
-      offset.value = {
-        x: e.translationX + start.value.x,
-        y: e.translationY + start.value.y
-      };
-    })
-    .onEnd(() => {
-      start.value = {
-        x: offset.value.x,
-        y: offset.value.y,
-      };
-    })
-    .onFinalize(() => {
-      isPressed.value = false;
-    });
+  const pan = Gesture.Pan()
+  .onUpdate((e) => {
+    offset.value = {
+      x: e.translationX + start.value.x,
+      y: e.translationY + start.value.y
+    };
+  })
+  .onEnd(() => {
+    start.value = {
+      x: offset.value.x,
+      y: offset.value.y,
+    };
+  });
 
-    const width = 960;
-    const height = 540;
+  const rotate = Gesture.Rotation()
+  .onUpdate((e)=>{
+    rotation.value = savedRotation.value + e.rotation;
+  })
+  .onEnd(()=>{
+    savedRotation.value = rotation.value;
+  });
 
-    const image = useImage(img);
+  const pinch = Gesture.Pinch()
+  .onUpdate((e)=>{ 
+    console.log("pinched")
+    scale.value = savedScale.value * e.scale;
+  })
+  .onEnd(()=>{
+    savedScale.value = scale.value;
+  })
 
-    return (
-      <View style={{flex:1}}>
-        <GestureDetector gesture={gesture}>
-            <Animated.View style={[styles.ball, animatedStyles]}>
-              <Canvas style={{width, height}}>
-                <Group>
-                  <Image x={0} y={0} image={image} width={width} height={height} fit="cover"/>
-                </Group>
-              </Canvas>
-            </Animated.View>
-        </GestureDetector>
-      </View>
-        
-    )
+  const width = 960;
+  const height = 540;
+
+  const image = useImage(img);
+
+  return (
+    <View style={{flex:1}}>
+      <GestureDetector gesture={Gesture.Race(pan, rotate, pinch)}>
+          <Animated.View style={[styles.ball, animatedStyles]}>
+            <Canvas style={{width, height}}>
+              <Group>
+                <Image x={0} y={0} image={image} width={width} height={height} fit="cover"/>
+              </Group>
+            </Canvas>
+          </Animated.View>
+      </GestureDetector>
+    </View>
+      
+  )
 }
 export default Map
