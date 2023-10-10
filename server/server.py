e8833e7be809d984026bddc5974025e2e5060d25
Henry Lam <hcylam@connect.ust.hk>
2023-10-10 13:43:26 +0800

    server now connects to a postgresql database rather than mysql

--- server/server.py
+++ server/server.py
@@ -3,6 +3,8 @@ from configs import Config
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 
+import psycopg2
+
 import datetime
 
 # flask server init
@@ -10,12 +12,19 @@ app = Flask(__name__)
 
 cors = CORS(app)
 
-app.config['MYSQL_HOST'] = Config.MYSQL_HOST
-app.config['MYSQL_USER'] = Config.MYSQL_USER
-app.config['MYSQL_PASSWORD'] = Config.MYSQL_PASSWORD
-app.config['MYSQL_DB'] = Config.MYSQL_DB
+# app.config['MYSQL_HOST'] = Config.MYSQL_HOST
+# app.config['MYSQL_USER'] = Config.MYSQL_USER
+# app.config['MYSQL_PASSWORD'] = Config.MYSQL_PASSWORD
+# app.config['MYSQL_DB'] = Config.MYSQL_DB
 
-mysql = MySQL(app)
+# mysql = MySQL(app)
+
+conn = psycopg2.connect(host=Config.PostgreSQL_HOST,
+                        database=Config.PostgreSQL_DATABASE,
+                        user=Config.PostgreSQL_USER,
+                        password=Config.PostgreSQL_PASSWORD)
+    
+
 
 # Format below:
 # [
@@ -41,21 +50,21 @@ mysql = MySQL(app)
 @app.route("/add_chairs", methods=["POST"])
 def add_chairs():
     # init variables
-    cursor = mysql.connection.cursor()
+    cursor = conn.cursor()
     user_input = request.json
 
     # insert chair into database for each chair passed by user.
     for i in range(len(user_input)):
         chair_id = user_input[i]["id"]
         date = user_input[i]["time"]
-        cursor.execute("SELECT * FROM chairs WHERE id = %s", [chair_id])
+        cursor.execute("SELECT * FROM fyp_db.chairs WHERE id = %s", [chair_id])
         found_chairs = cursor.fetchall()
 
         if (len(found_chairs) > 0):
             # delete old position in db of the current chair if the newest addition is newer.
             # since we always delete the old entry, there will always be 1 row returned in this case.
             if(found_chairs[0][6] < datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")):
-                cursor.execute("DELETE FROM chairs WHERE id = %s", [chair_id])
+                cursor.execute("DELETE FROM fyp_db.chairs WHERE id = %s", [chair_id])
                 mysql.connection.commit()
             else:
                 print("Most recently added chair location is outdated. Skipping.")
@@ -68,7 +77,7 @@ def add_chairs():
         zone = user_input[i]["zone"]
 
         # add new values to database.
-        cursor.execute("INSERT INTO chairs (status, x, y, type, zone, id, date) VALUES (%s, %s, %s, %s, %s, %s, %s)", [status, x, y, chair_type, zone, chair_id, date])
+        cursor.execute("INSERT INTO fyp_db.chairs (status, x, y, type, zone, id, date) VALUES (%s, %s, %s, %s, %s, %s, %s)", [status, x, y, chair_type, zone, chair_id, date])
     mysql.connection.commit()
     cursor.close()
 
@@ -76,8 +85,8 @@ def add_chairs():
 
 @app.route("/get_chairs", methods=["GET"])
 def get_chairs():
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM chairs")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM fyp_db.chairs")
     result = cursor.fetchall()
     cursor.close()
     return jsonify(result)
@@ -89,11 +98,11 @@ def get_chairs():
 @app.route("/get_chair_by_zone", methods=["POST"])
 def get_chair_by_zone():
     #init variables
-    cursor = mysql.connection.cursor()
+    cursor = conn.cursor()
     user_input = request.json
     zone = user_input["zone"]
 
-    cursor.execute("SELECT * FROM chairs WHERE zone = %s", [zone])
+    cursor.execute("SELECT * FROM fyp_db.chairs WHERE zone = %s", [zone])
     result = cursor.fetchall()
     cursor.close()
     return jsonify(result)
