725e021ffbd805f4f2981f5d886184ab88c639e5
Henry Lam <hcylam@connect.ust.hk>
2024-01-17 14:35:52 +0800

    removed old file, moved table_setup to routes

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 import routes.add_chairs
 import routes.get_chairs
 import routes.delete_chairs
-import db_config.table_setup
+import routes.table_setup
 import routes.add_tables
 import routes.get_tables
